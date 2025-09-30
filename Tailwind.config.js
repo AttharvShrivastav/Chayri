7b6f003489c043dc2d9743bd431d9f85a8efc688
Attharv <attharv.shrivastav@gmail.com>
2025-09-30 16:59:15 +0530

    Changes done.

--- Tailwind.config.js
+++ Tailwind.config.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 module.exports = {
 	darkMode: ['class'],
-	content: ['./src/**/*.{js,jsx,ts,tsx}'],
+	 content: [path.resolve(__dirname, 'src/**/*.{js,jsx,ts,tsx}')],
 	theme: {
 		container: {
 			center: true,
